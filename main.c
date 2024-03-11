fe1a6ea89908d8bd3807aeb255c2545fa5a44810
Maxluli <maxluligames@gmail.com>
2024-03-11 10:09:07 +0100

    aled

--- main.c
+++ main.c
@@ -4,14 +4,14 @@
 
 #include "utils/gfx/gfx.h"
 
-/*
+#include "utils/vec2/vec2.h"
+
 typedef struct
 {
     uint32_t row;
     uint32_t column;
-} coordinates_t;
-*/
+} caca;
 
-// void gfx_draw_line(struct gfx_context_t *ctxt, coordinates_t p0, coordinates_t p1, uint32_t color);
+void gfx_draw_line(struct gfx_context_t *ctxt, caca p0, caca p1, uint32_t color);
 
-// void gfx_draw_circle(struct gfx_context_t *ctxt, coordinates_t c, uint32_t r, uint32_t color);
+void gfx_draw_circle(struct gfx_context_t *ctxt, caca c, uint32_t r, uint32_t color);
