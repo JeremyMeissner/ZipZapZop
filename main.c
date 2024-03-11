4038df1b4402da983d69a9b57dcb5ec4ae3612fb
Jeremy Meissner <jymeissner@gmail.com>
2024-03-11 09:55:07 +0100

    feat: add makefile and other things

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdint.h>
+
+#include "utils/utils.h"
+
+#include "utils/gfx/gfx.h"
+
+typedef struct
+{
+    uint32_t row;
+    uint32_t column;
+} coordinates_t;
+
+// void gfx_draw_line(struct gfx_context_t *ctxt, coordinates_t p0, coordinates_t p1, uint32_t color);
+
+// void gfx_draw_circle(struct gfx_context_t *ctxt, coordinates_t c, uint32_t r, uint32_t color);
